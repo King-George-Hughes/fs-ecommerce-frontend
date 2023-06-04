4c6f640dc22ca8b8bd46c374232424f7c251fe59
unknown <hughesgeorgeking@gmail.com>
2023-06-04 17:46:59 +0000

    changes

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,15 +1,16 @@
 import { useStateContext } from "@/lib/context";
 import { FaShoppingCart } from "react-icons/fa";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 
 const Cart = () => {
-  const { cartItems } = useStateContext();
+  const { cartItems, setShowCart } = useStateContext();
 
   return (
     <div className="fixed w-full h-full left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.8)]">
       <div className="fixed right-0 w-[40%] h-full bg-[#f1f1f1] p-10 md:p-20">
         {cartItems.length < 1 && (
           <div className="w-full h-full flex flex-col items-center justify-center">
-            <h1 className="text-lg font-medium mb-10">
+            <h1 className="text-lg font-bold mb-10">
               You have more shopping to do 🛒😊
             </h1>
             <FaShoppingCart size={150} color="#444" />
@@ -27,6 +28,16 @@ const Cart = () => {
                 <div className="w-full h-full flex flex-col justify-center items-center">
                   <h3 className="text-lg font-bold">{item.title}</h3>
                   <h3>{item.price}</h3>
+                  <span className="mr-4">Quantity</span>
+                  <div className="flex items-center justify-center my-2">
+                    <button>
+                      <AiFillMinusCircle size={20} />
+                    </button>
+                    <span className="mx-3">0</span>
+                    <button>
+                      <AiFillPlusCircle size={20} />
+                    </button>
+                  </div>
                 </div>
               </div>
             );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
 import { Cart } from ".";
+import { useStateContext } from "@/lib/context";
+import { useState } from "react";
 
 const Navbar = () => {
+  //   const [showCart, setShowCart] = useStateContext();
+  const [showTheCart, setShowTheCart] = useState(false);
+
   return (
     <nav className="fixed w-full h-[60px] bg-white z-10">
       <div className="container mx-auto max-w-[1200px] h-full px-5 py-3 flex items-center justify-between">
@@ -10,12 +15,16 @@ const Navbar = () => {
           Shopping
         </Link>
 
-        <div className="flex flex-col items-center font-medium">
+        <div
+          className="flex flex-col items-center font-medium cursor-pointer"
+          onClick={() => setShowTheCart(true)}
+        >
           <FiShoppingBag />
           <span>Cart</span>
         </div>
 
-        <Cart />
+        {/* {showCart && <Cart />} */}
+        {showTheCart && <Cart />}
       </div>
     </nav>
   );
